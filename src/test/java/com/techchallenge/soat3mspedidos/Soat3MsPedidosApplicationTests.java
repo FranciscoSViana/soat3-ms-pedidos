8f418d402685cb6ae58d01c3719ff725cbb74b3d
Francisco Viana <franciscosantos29@gmail.com>
2024-05-14 21:02:40 -0300

    criação manifestos yaml

--- src/test/java/com/techchallenge/soat3mspedidos/Soat3MsPedidosApplicationTests.java
+++ src/test/java/com/techchallenge/soat3mspedidos/Soat3MsPedidosApplicationTests.java
@@ -1,13 +1,12 @@
 package com.techchallenge.soat3mspedidos;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Soat3MsPedidosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
